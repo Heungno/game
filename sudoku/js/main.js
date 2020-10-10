cf6094832f18125ad449627c7d3d33abc286cab1
heungno <gmd5366@gmail.com>
2020-10-10 11:50:57 +0900

    ggg

--- sudoku/js/main.js
+++ sudoku/js/main.js
@@ -3,6 +3,7 @@
 function shwPlayTime() {
   const playTimeElementer = document.querySelector('.title>h2>span');
   playTimeElementer.innerText = game.getPlayTime();
+  console.log(`f`)
 }
 
 const game = new sudoku(1, 9, null);
